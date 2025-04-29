93af26735fbdba209b439bc7d44ffe26532cce62
hempflower <1993996310@qq.com>
2025-04-29 16:17:55 +0800

    feat: 重新组织代码结构，并添加更多功能

--- cmd/btcli/main.go
+++ cmd/btcli/main.go
@@ -2,78 +2,12 @@ package main
 
 import (
 	"context"
-	"crypto/md5"
-	"crypto/tls"
-	"encoding/hex"
-	"fmt"
 	"log"
-	"net/http"
-	"net/url"
 	"os"
-	"time"
 
-	"github.com/carlmjohnson/requests"
+	"github.com/hempflower/btcli/internal/btapi"
 	"github.com/urfave/cli/v3"
 )
-
-type BtApiClient struct {
-	btUrl              string
-	btKey              string
-	ignoreSslTransport *http.Transport
-}
-
-// 生成 request_token: md5(request_time + md5(api_sk))
-func (bt *BtApiClient) GenerateRequestToken(requestTime time.Time) string {
-	timestamp := fmt.Sprintf("%d", requestTime.Unix()) // Unix 时间戳（秒）
-	innerMd5 := md5.Sum([]byte(bt.btKey))              // md5(api_sk)
-	innerMd5Str := hex.EncodeToString(innerMd5[:])     // 转为字符串
-	tokenSource := timestamp + innerMd5Str             // 拼接字符串
-	token := md5.Sum([]byte(tokenSource))              // md5(timestamp + md5(api_sk))
-	return hex.EncodeToString(token[:])                // 返回最终的 token
-}
-
-func (bt *BtApiClient) AppendSignatureBody(body url.Values) {
-	now := time.Now()
-	body.Set("request_time", fmt.Sprintf("%d", now.Unix()))
-	body.Set("request_token", bt.GenerateRequestToken(now))
-}
-
-type RestartGoProjectResponse struct {
-	Status     bool   `json:"status"`
-	StatusCode int    `json:"status_code"`
-	ErrorMsg   string `json:"error_msg"`
-	Data       string `json:"data"`
-}
-
-func (bt *BtApiClient) RestartGoProject(name string) (*RestartGoProjectResponse, error) {
-	var err error
-	postForm := url.Values{}
-	postForm.Set("project_name", name)
-	bt.AppendSignatureBody(postForm)
-	var response RestartGoProjectResponse
-	err = requests.
-		URL(bt.btUrl).
-		Transport(bt.ignoreSslTransport).
-		Path("/project/go/restart_project").
-		Method("POST").
-		BodyForm(postForm).
-		ToJSON(&response).
-		Fetch(context.Background())
-	if err != nil {
-		return nil, err
-	}
-	return &response, nil
-}
-
-func NewBtApiClient(btUrl string, btKey string) *BtApiClient {
-	return &BtApiClient{
-		btUrl: btUrl,
-		btKey: btKey,
-		ignoreSslTransport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-		},
-	}
-}
 
 func main() {
 	cmd := &cli.Command{
@@ -96,6 +30,52 @@ func main() {
 				Name: "go-project",
 				Commands: []*cli.Command{
 					{
+						Name: "start",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:     "name",
+								Usage:    "The name of the go project",
+								Required: true,
+							},
+						},
+						Action: func(ctx context.Context, cmd *cli.Command) error {
+							bt := btapi.NewBtApiClient(cmd.String("bt-url"), cmd.String("bt-key"))
+							response, err := bt.StartGoProject(cmd.String("name"))
+							if err != nil {
+								return err
+							}
+							if !response.Status {
+								log.Fatal(response.ErrorMsg)
+							} else {
+								log.Println(response.Data)
+							}
+							return nil
+						},
+					},
+					{
+						Name: "stop",
+						Flags: []cli.Flag{
+							&cli.StringFlag{
+								Name:     "name",
+								Usage:    "The name of the go project",
+								Required: true,
+							},
+						},
+						Action: func(ctx context.Context, cmd *cli.Command) error {
+							bt := btapi.NewBtApiClient(cmd.String("bt-url"), cmd.String("bt-key"))
+							response, err := bt.StopGoProject(cmd.String("name"))
+							if err != nil {
+								return err
+							}
+							if !response.Status {
+								log.Fatal(response.ErrorMsg)
+							} else {
+								log.Println(response.Data)
+							}
+							return nil
+						},
+					},
+					{
 						Name: "restart",
 						Flags: []cli.Flag{
 							&cli.StringFlag{
@@ -105,7 +85,7 @@ func main() {
 							},
 						},
 						Action: func(ctx context.Context, cmd *cli.Command) error {
-							bt := NewBtApiClient(cmd.String("bt-url"), cmd.String("bt-key"))
+							bt := btapi.NewBtApiClient(cmd.String("bt-url"), cmd.String("bt-key"))
 							response, err := bt.RestartGoProject(cmd.String("name"))
 							if err != nil {
 								return err
---
+++ internal/btapi/client.go
@@ -0,0 +1,122 @@
+package btapi
+
+import (
+	"context"
+	"crypto/md5"
+	"crypto/tls"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/carlmjohnson/requests"
+)
+
+type BtApiClient struct {
+	btUrl              string
+	btKey              string
+	ignoreSslTransport *http.Transport
+}
+
+// 生成 request_token: md5(request_time + md5(api_sk))
+func (bt *BtApiClient) GenerateRequestToken(requestTime time.Time) string {
+	timestamp := fmt.Sprintf("%d", requestTime.Unix()) // Unix 时间戳（秒）
+	innerMd5 := md5.Sum([]byte(bt.btKey))              // md5(api_sk)
+	innerMd5Str := hex.EncodeToString(innerMd5[:])     // 转为字符串
+	tokenSource := timestamp + innerMd5Str             // 拼接字符串
+	token := md5.Sum([]byte(tokenSource))              // md5(timestamp + md5(api_sk))
+	return hex.EncodeToString(token[:])                // 返回最终的 token
+}
+
+func (bt *BtApiClient) AppendSignatureBody(body url.Values) {
+	now := time.Now()
+	body.Set("request_time", fmt.Sprintf("%d", now.Unix()))
+	body.Set("request_token", bt.GenerateRequestToken(now))
+}
+
+type RestartGoProjectResponse struct {
+	Status     bool   `json:"status"`
+	StatusCode int    `json:"status_code"`
+	ErrorMsg   string `json:"error_msg"`
+	Data       string `json:"data"`
+}
+
+func (bt *BtApiClient) InvokeBtApi(path string, body url.Values, resp any) error {
+	var err error
+	bt.AppendSignatureBody(body)
+	err = requests.
+		URL(bt.btUrl).
+		Transport(bt.ignoreSslTransport).
+		Path(path).
+		Method(http.MethodPost).
+		BodyForm(body).
+		ToJSON(resp).
+		Fetch(context.Background())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (bt *BtApiClient) RestartGoProject(name string) (*RestartGoProjectResponse, error) {
+	var err error
+	postForm := url.Values{}
+	postForm.Set("project_name", name)
+	var response RestartGoProjectResponse
+	err = bt.InvokeBtApi("/project/go/restart_project", postForm, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+type StartGoProjectResponse struct {
+	Status     bool   `json:"status"`
+	StatusCode int    `json:"status_code"`
+	ErrorMsg   string `json:"error_msg"`
+	Data       string `json:"data"`
+}
+
+func (bt *BtApiClient) StartGoProject(name string) (*StartGoProjectResponse, error) {
+	var err error
+	postForm := url.Values{}
+	postForm.Set("project_name", name)
+	var response StartGoProjectResponse
+	err = bt.InvokeBtApi("/project/go/start_project", postForm, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
+
+type StopGoProjectResponse struct {
+	Status     bool   `json:"status"`
+	StatusCode int    `json:"status_code"`
+	ErrorMsg   string `json:"error_msg"`
+	Data       string `json:"data"`
+}
+
+func (bt *BtApiClient) StopGoProject(name string) (*StopGoProjectResponse, error) {
+	var err error
+	postForm := url.Values{}
+	postForm.Set("project_name", name)
+	var response StopGoProjectResponse
+	err = bt.InvokeBtApi("/project/go/stop_project", postForm, &response)
+	if err != nil {
+		return nil, err
+	}
+	return &response, nil
+}
+
+func NewBtApiClient(btUrl string, btKey string) *BtApiClient {
+	return &BtApiClient{
+		btUrl: btUrl,
+		btKey: btKey,
+		ignoreSslTransport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	}
+}
